f9563e6612ce305b7a6661c007a19b99d2ac4323
MatrixMike <matroxmike@icloud.com>
2016-03-10 11:51:11 +1100

    now using clojure 1.8

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject javaxSwingSMD "1.0.2-SNAPSHOT"
+(defproject javaxSwingSMD "1.0.3-SNAPSHOT"
   :description "creates window on screen with user info"
-  :dependencies [[org.clojure/clojure "1.3.0"]]
+  :dependencies [[org.clojure/clojure "1.8.0"]]
   
   :main javaxSwingSMD.core
   
